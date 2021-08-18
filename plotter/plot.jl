476f5a3cc64b92a269bebe91a70969b988c105a6
fredrik <freddahf@outlook.com>
2021-08-18 19:36:53 +0200

    cool stuffies

--- plotter/plot.jl
+++ plotter/plot.jl
@@ -1,14 +1,15 @@
 import CSV
 using Plots
 
+println("reading from file")
 data = CSV.File("log.csv")
 
+println("filtering bedroom_data")
 bedroom_data = filter(data -> data[2] == "Bedroom", data)
-
-dt = map(data -> data[1], bedroom_data)
-temp = map(data -> data[3], bedroom_data)
+livingr_data = filter(data -> data[2] == "Livingroom", data)
 
 gr()
-p = plot(dt, temp)
+p = plot(map(data -> data[1], bedroom_data), map(data -> data[3], bedroom_data))
+plot!(p, map(data -> data[1], livingr_data), map(data -> data[3], livingr_data))
 
-savefig(p, "test.pdf")
+savefig(p, "plot.pdf")
