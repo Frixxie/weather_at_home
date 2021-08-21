c72ec4c94b5134f64f7f5cf1064db0dd1959fb7b
fredrik <freddahf@outlook.com>
2021-08-21 10:06:39 +0200

    cool stuffies

--- plotter/plot.jl
+++ plotter/plot.jl
@@ -1,15 +1,21 @@
 import CSV
-using Plots
+import Plots
 
-println("reading from file")
+println("reading from log.csv")
 data = CSV.File("log.csv")
+
+println(data)
 
 println("filtering bedroom_data")
 bedroom_data = filter(data -> data[2] == "Bedroom", data)
+println("filtering livingr_data")
 livingr_data = filter(data -> data[2] == "Livingroom", data)
 
-gr()
-p = plot(map(data -> data[1], bedroom_data), map(data -> data[3], bedroom_data))
-plot!(p, map(data -> data[1], livingr_data), map(data -> data[3], livingr_data))
+println("setting up backend")
+Plots.gr()
 
-savefig(p, "plot.pdf")
+println("Plotting")
+p = Plots.plot(map(data -> data[1], bedroom_data), map(data -> data[3], bedroom_data))
+Plots.plot!(p, map(data -> data[1], livingr_data), map(data -> data[3], livingr_data))
+
+Plots.savefig(p, "plot.pdf")
